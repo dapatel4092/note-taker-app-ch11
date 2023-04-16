7cac3b50023ee9effd10ca7861b4594a3a546442
Dhruv <dapatel700@gmail.com>
2023-04-16 16:37:20 -0700

    finished routes js files localhost is also up and running

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,7 +1,22 @@
 const app = require("express").Router()
 const fs = require('fs')
 let db = require('../db/db.json')
-
+// CRUD 
+//Get - READ
 app.get("/api/notes",(req,res) => {
-    res.sendFile(path.join(__dirname,"../public/index.html"))
+    db = JSON.parse(fs.readFileSync("./db/db.json")) || []
+    res.json(db)
 })
+app.post("/api/notes",(req,res) => {
+    const creatdata = {
+        id: Math.floor(Math.random()* 9999),
+        title: req.body.title, text: req.body.text
+    }
+    db.push(creatdata) 
+    
+    fs.writeFileSync("./db/db.json",JSON.stringify(db),function(err){
+        if(err) throw err
+    }) 
+    res.json(db)
+})
+module.exports = app
